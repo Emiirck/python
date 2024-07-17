33a5e141ff36fc180b88df3e59f1cb6432a76147
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-07-16 19:57:51 -0500

    Matrix

---
+++ LAMatrix.py
@@ -0,0 +1,6 @@
+# Escribe un programa que solicite al usuario que ingrese su nombr. El nombre se debe almacenar en una variable
+# llamada nombre. A continuacion se debe mostrar en pantalla el texto "Ahora estás en la matrix" [Usuario]
+# donde, [Usuario] se reemplazara por el nombre de usuario escrito
+
+nombre = input("Escriba su nombre:")
+print("Ahora estas en la matrix  "  + nombre + " >:)")
