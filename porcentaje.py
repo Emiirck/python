4cb3f21c125a02ecf01aae0b4c639a1855964575
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:02:26 -0500

    porcentaje

---
+++ porcentaje.py
@@ -0,0 +1,6 @@
+# Escribe un programa que solicie al usuario un número y le reste el 15%, almacenando todo en una única variable.
+# A continuación. mostrar el resulado final en pantalla.
+
+número= int(input("Digite un número: "))
+porcentaje = número - (número * 0.15)
+print(porcentaje)
